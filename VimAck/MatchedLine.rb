625623497b8b3b947454bd93d8e96410809434b1
Majd Taby <mtaby@me.com>
2011-05-28 16:33:45 -0700

    now showing results and clicking on them opens the file at the location in macvim

---
+++ VimAck/MatchedLine.rb
@@ -0,0 +1,11 @@
+#
+#  MatchedLine.rb
+#  VimAck
+#
+#  Created by Majd Taby on 5/28/11.
+#  Copyright 2011 University of Michigan - Dearborn. All rights reserved.
+#
+
+class MatchedLine
+    attr_accessor :filename, :line_number, :matched_line, :matched_ranges, :query
+end
